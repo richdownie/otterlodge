814c45911104528ff9965701178162ffd451f1e8
rich <rich@c5cf4f53-3e1b-0410-9ef4-e74721d9d921>
2006-12-07 02:13:24 +0000

    git-svn-id: https://secure.svnrepository.com/s_digger/rudyonrails/otterlodge/trunk@336 c5cf4f53-3e1b-0410-9ef4-e74721d9d921

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -4,7 +4,11 @@ class EventTest < Test::Unit::TestCase
   fixtures :events
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_invalid_with_empty_attributes
+    event = Event.new
+    assert !event.valid?
+    assert event.errors.invalid?(:name)
+    assert event.errors.invalid?(:date)
   end
+  
 end
