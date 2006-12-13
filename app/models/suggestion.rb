cfde53cd576963198af417f304f72f728b9d9309
rich <rich@c5cf4f53-3e1b-0410-9ef4-e74721d9d921>
2006-12-13 02:02:35 +0000

    git-svn-id: https://secure.svnrepository.com/s_digger/rudyonrails/otterlodge/trunk@405 c5cf4f53-3e1b-0410-9ef4-e74721d9d921

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,5 +1,5 @@
 class Suggestion < ActiveRecord::Base
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :on => :create
   validates_presence_of :name
-  validates_presence_of :name
+  validates_presence_of :email
 end
