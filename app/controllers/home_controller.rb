e58fde7a76b086e743f814247f2c7b2a9d80e1b4
rich <rich@c5cf4f53-3e1b-0410-9ef4-e74721d9d921>
2006-12-16 16:40:48 +0000

    git-svn-id: https://secure.svnrepository.com/s_digger/rudyonrails/otterlodge/trunk@457 c5cf4f53-3e1b-0410-9ef4-e74721d9d921

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
 
   def list
     @specials = Special.current_specials
-    @events = Event.current_events    
+    @events = Event.current_events
   end
 end
