08357b7734700ec6c9b50693747dac56d1316aa5
rich <rich@c5cf4f53-3e1b-0410-9ef4-e74721d9d921>
2006-12-16 16:41:00 +0000

    git-svn-id: https://secure.svnrepository.com/s_digger/rudyonrails/otterlodge/trunk@458 c5cf4f53-3e1b-0410-9ef4-e74721d9d921

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,8 @@ class Event < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :date
 
-  def self.current_events
-    find(:all, :order => "date asc")
+  def self.current_events 
+    find(:all, :conditions => "date > '#{Time.now.to_s(:db)}'", :order => "date asc" )
   end
   
 end
