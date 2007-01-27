e6f911f30ea0bf5fe5499873fcfaa693aac9b8f0
rich <rich@c5cf4f53-3e1b-0410-9ef4-e74721d9d921>
2007-01-27 13:36:21 +0000

    git-svn-id: https://secure.svnrepository.com/s_digger/rudyonrails/otterlodge/trunk@657 c5cf4f53-3e1b-0410-9ef4-e74721d9d921

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   def list
     @specials = Special.current_specials
     @events = Event.current_events
-    page.visual_effect :grow, 'bears'
     
   end
 end
