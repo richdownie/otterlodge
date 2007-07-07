8ebf2d252a41730320c0ed47d115db007ed492fa
rich <rich@c5cf4f53-3e1b-0410-9ef4-e74721d9d921>
2007-07-07 17:08:33 +0000

    update
    git-svn-id: https://secure.svnrepository.com/s_digger/rudyonrails/otterlodge/trunk@799 c5cf4f53-3e1b-0410-9ef4-e74721d9d921

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   def list
     @specials = Special.current_specials
     @events = Event.current_events
-    
+  end
+  
+  def rewards
+     render :template => "home/rewards", :layout => false
   end
 end
