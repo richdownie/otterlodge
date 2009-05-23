3bb9d03f36f2b5c3c9f7625cf3677547d3e3f909
Jeff Whitmire <jeff@jwhitmire.com>
2009-05-23 00:02:20 -0400

    Application Controller consolidation

--- app/controllers/application.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::Base
-  include AuthenticatedSystem
-
-  # If you want "remember me" functionality, add this before_filter to Application Controller
-  before_filter :login_from_cookie
-end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  include AuthenticatedSystem
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # If you want "remember me" functionality, add this before_filter to Application Controller
+  before_filter :login_from_cookie
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
