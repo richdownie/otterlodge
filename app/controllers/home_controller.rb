41e2a58419b3e45467d1ff5c11d41b106bb8d4da
Jeff Whitmire <jeff@jwhitmire.com>
2009-05-23 11:58:50 -0400

    add action caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
 
+  # caches_page :list
+  caches_action :list
+  
   def list
     @specials = Special.current_specials
     @events = Event.current_events
